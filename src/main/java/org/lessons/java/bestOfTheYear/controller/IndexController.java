c49a7263a8334b61cc65c4f5f1f68b635ec53e84
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-07 14:36:37 +0100

    add method getName

--- src/main/java/org/lessons/java/bestOfTheYear/controller/IndexController.java
+++ src/main/java/org/lessons/java/bestOfTheYear/controller/IndexController.java
@@ -1,9 +1,17 @@
 package org.lessons.java.bestOfTheYear.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+    @GetMapping("home")
+    public String getName(Model model){
+        String name = "Giovanni";
+        model.addAttribute("myName", name);
+        return "home-page";
+    }
 }
