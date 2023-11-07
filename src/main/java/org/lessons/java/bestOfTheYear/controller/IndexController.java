7a17e633356c1a01aa6f490c4bea3fa528495ae6
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-07 14:25:02 +0100

    Add controller and request mapping

--- src/main/java/org/lessons/java/bestOfTheYear/controller/IndexController.java
+++ src/main/java/org/lessons/java/bestOfTheYear/controller/IndexController.java
@@ -1,4 +1,9 @@
 package org.lessons.java.bestOfTheYear.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class IndexController {
 }
