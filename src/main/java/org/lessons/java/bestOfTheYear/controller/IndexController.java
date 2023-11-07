aa20ac65a2061eb50493700fffb41c2b8a99eeb7
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-07 14:52:53 +0100

    Add priv method get Best MMovies

--- src/main/java/org/lessons/java/bestOfTheYear/controller/IndexController.java
+++ src/main/java/org/lessons/java/bestOfTheYear/controller/IndexController.java
@@ -4,14 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+    //Index
+    @GetMapping
+    @ResponseBody
+    public String index(){
+        return "Ciao ciao, sono la index!";
+    }
+
+    // Homepage
     @GetMapping("home")
     public String getName(Model model){
         String name = "Giovanni";
         model.addAttribute("myName", name);
         return "home-page";
+    }
+
+    private List<String> getBestMovies() {
+        List<String> bestMovies = new ArrayList<>();
     }
 }
