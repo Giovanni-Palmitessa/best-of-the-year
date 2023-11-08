871c6f9ca7a7f366b23edd777237e6e4b7e61685
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-08 14:14:01 +0100

    add attributes and constructor

--- src/main/java/org/lessons/java/bestOfTheYear/model/Movie.java
+++ src/main/java/org/lessons/java/bestOfTheYear/model/Movie.java
@@ -1,4 +1,14 @@
 package org.lessons.java.bestOfTheYear.model;
 
 public class Movie {
+    // Attributi
+    private int id;
+    private String title;
+
+    // Costruttori
+
+    public Movie(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
 }
