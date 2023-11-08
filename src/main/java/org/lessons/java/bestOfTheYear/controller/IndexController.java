c7f47bd56789745d8c2c440468fddad0628d3180
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-08 15:31:29 +0100

    add method getMoviebycode ande movieDetail Route

--- src/main/java/org/lessons/java/bestOfTheYear/controller/IndexController.java
+++ src/main/java/org/lessons/java/bestOfTheYear/controller/IndexController.java
@@ -5,6 +5,7 @@ import org.lessons.java.bestOfTheYear.model.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -32,29 +33,48 @@ public class IndexController {
 
     // get Movies
     private List<Movie> getBestMovies() {
-        Movie[] bestMoviesArray = {new Movie(1, "Avengers: Endgame"), new Movie(2, "Avengers: Infinity War"), new Movie(3, "Avengers")};
+        Movie[] bestMoviesArray = {new Movie("1", "Avengers: Endgame"), new Movie("2", "Avengers: Infinity War"), new Movie("3", "Avengers")};
         return Arrays.asList(bestMoviesArray);
     }
 
     // get Songs
     private List<Song> getBestSongs() {
-        Song[] bestSongsArray = {new Song(4, "Bad Bunny - La Jumpa"), new Song(5, "Rick Astley - Never Gonna Give You Up"), new Song(6, "Bello Figo Gu - Pasta con tonno")};
+        Song[] bestSongsArray = {new Song("4", "Bad Bunny - La Jumpa"), new Song("5", "Rick Astley - Never Gonna Give You Up"), new Song("6", "Bello Figo Gu - Pasta con tonno")};
         return Arrays.asList(bestSongsArray);
     }
 
+    // get Movie id
+    private Movie getMovieById(String id) {
+
+        for (Movie movie : getBestMovies()) {
+            if (movie.getId().equals(id)) {
+                return movie;
+            }
+        }
+        return null;
+    }
+
     // movies.html
-    @GetMapping("movies")
+    @GetMapping("/movies")
     public String movies(Model model){
         List<Movie> bestMoviesArray = getBestMovies();
         model.addAttribute("getBestMovies", bestMoviesArray);
         return "movies";
     }
 
-    // songs
-    @GetMapping("songs")
+    // songs.html
+    @GetMapping("/songs")
     public String songs(Model model){
         List<Song> bestSongsArray = getBestSongs();
         model.addAttribute("getBestSongs", bestSongsArray);
         return "songs";
+    }
+
+    // movie-detail.html
+    @GetMapping("/movies/{code}") // students/123
+    public String movieDetail(@PathVariable("id") String movieId, Model model) {
+        Movie movie = getMovieById(movieId);
+        model.addAttribute("movie", movie);
+        return "movie-detail";
     }
 }
